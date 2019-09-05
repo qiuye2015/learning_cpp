5c36e8aefc1cb873880740222ff2207eca9c7812
qiuye2015 <1119345739@qq.com>
2019-09-05 14:32:43 +0800

    TODO

--- boost/build.sh
+++ boost/build.sh
@@ -1,16 +1,8 @@
-BOOST_HOME_2=/home/fjp/bin/boost_1_69_bin
-CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$BOOST_HOME_2/include
-export CPLUS_INCLUDE_PATH
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BOOST_HOME_2/lib
-export LD_LIBRARY_PATH
-LIBRARY_PATH=$LIBRARY_PATH:$BOOST_HOME_2/lib
-export LIBRARY_PATH
-
-if [[ $# -ge 2 ]];then
+#!/bin/bash
+BOOST_HOME=/home/work/fjp/lib/boost_1_69_0_bin
+if [ "$#" == "2" ];then
 input=$1
 output=$2
-
-#output='test'
-g++ -std=c++11 -I $BOOST_HOME_2/include  -lboost_thread -lpthread -g -o $output $input
+gcc -std=c++11 -I $BOOST_HOME/include -L $BOOST_HOME/lib -lboost_thread -lboost_serialization -lboost_program_options -g -o $output $input
 echo "success!!"
 fi
--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-#g++ $1 -g -o test -lboost_thread -lpthread
 g++ $1 -g -o test -ltcmalloc -lprofiler
