3caf6025665f2c1e0df1b8e28e4d3422293e1f10
qiuye2015 <1119345739@qq.com>
2019-09-23 20:42:38 +0800

    not test

--- boost/build.sh
+++ boost/build.sh
@@ -3,6 +3,7 @@ BOOST_HOME=/home/work/fjp/lib/boost_1_69_0_bin
 if [ "$#" == "2" ];then
 input=$1
 output=$2
-gcc -std=c++11 -I $BOOST_HOME/include -L $BOOST_HOME/lib -lboost_thread -lboost_serialization -lboost_program_options -g -o $output $input
+g++ -std=c++11 -I $BOOST_HOME/include -L $BOOST_HOME/lib -lboost_thread -lpthread -lboost_serialization -lboost_program_options -g -o $output $input
+#gcc -std=c++11 -I $BOOST_HOME/include -L $BOOST_HOME/lib -lboost_thread -lboost_serialization -lboost_program_options -g -o $output $input
 echo "success!!"
 fi
